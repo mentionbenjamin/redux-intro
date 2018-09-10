220a790e6f5fd268f595cc2b74e2aa68d3b5fe22
Ben Robinson <mentionbenjamin@gmail.com>
2018-09-10 11:05:44 +0100

    receives response back from post

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,24 +4,57 @@ class PostForm extends Component {
 
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      title: '',
+      body: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
-  render() {
+  onChange(e){
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
+  onSubmit(e){
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(post)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
+
+  render() {
     return(
       <div>
         <h1>Add Post</h1>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div>
-            <label>Title:</label>
-            <br/>
-            <input type="text" name="title"/>
+            <label>Title:</label><br/>
+            <input type="text"
+                   name="title"
+               onChange={this.onChange}
+                  value={this.state.title}/>
           </div>
           <div>
-            <label>Body:</label>
-            <br/>
-            <input type="text" name="body"/>
+            <label>Body:</label><br/>
+            <input type="text"
+                   name="body"
+               onChange={this.onChange}
+                  value={this.state.body}/>
           </div>
           <button type="submit">Submit</button>
         </form>
